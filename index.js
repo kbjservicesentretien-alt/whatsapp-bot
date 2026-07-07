89dc3b324cfbcfd6e85b72cb979974668ed1ccbb
kbjservicesentretien-alt <kbjservicesentretien@gmail.com>
2026-07-07 22:02:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Serveur lancé sur le port", PORT);
 });
